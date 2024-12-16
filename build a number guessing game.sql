19c2b2cbbc248d6742fe6dd930e00204443e38ed
jbaomnegs <jbaomnegs5513@gmail.com>
2024-12-16 22:12:26 +0800

    Update build a number guessing game.sql

--- build a number guessing game.sql
+++ build a number guessing game.sql
@@ -3,39 +3,39 @@
 # This is a simple Number Guessing Game script
 echo "Welcome to the Number Guessing Game!"
 
-# 連接資料庫的 PSQL 命令
+# PSQL commands to connect to the database
 PSQL="psql --username=freecodecamp --dbname=number_guess -t --no-align -c"
 
-# 隨機生成 1 到 1000 的數字
+# Randomly generate a number from 1 to 1000
 SECRET_NUMBER=$(( RANDOM % 1000 + 1 ))
 
-# 計算用戶猜測的次數
+# Count the number of guesses made by the user
 NUMBER_OF_GUESSES=0
 
-# 提示用戶輸入用戶名
+# Prompt user to enter username
 echo "Enter your username:"
 read USERNAME
 
-# 查詢用戶資料
+# Query user information
 USER_INFO=$($PSQL "SELECT games_played, best_game FROM users WHERE username='$USERNAME'")
 
-# 判斷用戶是否存在
+# Determine whether the user exists
 if [[ -z $USER_INFO ]]; then
   echo "Welcome, $USERNAME! It looks like this is your first time here."
-  # 新用戶插入資料庫
+  # Insert new user into database
   INSERT_USER=$($PSQL "INSERT INTO users(username) VALUES('$USERNAME')")
 else
-  # 已存在用戶，顯示歡迎信息
+  # User already exists, display welcome message
   IFS="|" read GAMES_PLAYED BEST_GAME <<< "$USER_INFO"
   echo "Welcome back, $USERNAME! You have played $GAMES_PLAYED games, and your best game took $BEST_GAME guesses."
 fi
 
-# 開始猜數字遊戲
+# Start guessing number game
 echo "Guess the secret number between 1 and 1000:"
 while true; do
   read GUESS
 
-  # 檢查輸入是否為整數
+  # Checks if the input is an integer
   if [[ ! $GUESS =~ ^[0-9]+$ ]]; then
     echo "That is not an integer, guess again:"
   else
@@ -46,9 +46,9 @@ while true; do
       echo "It's lower than that, guess again:"
     else
       echo "You guessed it in $NUMBER_OF_GUESSES tries. The secret number was $SECRET_NUMBER. Nice job!"
-      # 更新資料庫中的遊戲記錄
+      # Update game records in the database
       UPDATE_RESULT=$($PSQL "UPDATE users SET games_played = games_played + 1 WHERE username='$USERNAME'")
-      # 更新最佳遊戲記錄
+      # Update best game record
       if [[ -z $BEST_GAME || $NUMBER_OF_GUESSES -lt $BEST_GAME ]]; then
         UPDATE_BEST=$($PSQL "UPDATE users SET best_game = $NUMBER_OF_GUESSES WHERE username='$USERNAME'")
       fi
